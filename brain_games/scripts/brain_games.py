fd24db03f5470f88fea6f213edadd31d0d9bce50
Fedor Savin <Savin20153@gmail.com>
2025-02-02 21:59:20 +0300

    lint by ruff

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,17 @@
 import sys
+
 sys.path.append('/home/fedorpc/projects/python-project-49/brain_games')
 from cli import welcome_user
+
 
 def greet():
     print("Welcome to the Brain Games!")
 
+
 def main():
     greet()
     welcome_user()
+
 
 if __name__ == "__main__":
     main()
