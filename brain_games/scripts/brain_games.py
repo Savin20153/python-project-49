e80e3a434f4d3e238ca438c6a6aafa55d55df59a
Fedor Savin <Savin20153@gmail.com>
2025-02-02 18:14:06 +0300

    git add welcome_user() in main

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
+import sys
+sys.path.append('/home/fedorpc/projects/python-project-49/brain_games')
+from cli import welcome_user
+
 def greet():
     print("Welcome to the Brain Games!")
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == "__main__":
     main()
