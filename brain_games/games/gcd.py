fc1e995eeb564d56f3fc521a09ebc12c32619a10
Fedor Savin <Savin20153@gmail.com>
2025-02-09 16:55:33 +0300

    update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,19 +3,17 @@ import random
 QUESTION = 'Find the greatest common divisor of given numbers.'
 
 
+def get_divisors(number):
+    return [div for div in range(1, number + 1) if number % div == 0]
+
+
 def ques_num_and_ans():
     num_one, num_two = random.randint(1, 100), random.randint(1, 100)
-    num_one_divs, num_two_divs = [], []
+    num_one_dlvs = get_divisors(num_one)
+    num_two_dlvs = get_divisors(num_two)
     
-    for div_one in range(1, num_one + 1):
-        if num_one % div_one == 0:
-            num_one_divs.append(div_one)
+    common_divisors = set(num_one_dlvs).intersection(num_two_dlvs)
+    right_ans = str(max(common_divisors))
     
-    for div_two in range(1, num_two + 1):
-        if num_two % div_two == 0:
-            num_two_divs.append(div_two)
-    
-    intersection_list = set(num_one_divs).intersection(num_two_divs)
-    right_ans = str(max(intersection_list))
     ques_num = f'{num_one} {num_two}' 
     return ques_num, right_ans
