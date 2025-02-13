dbd815e9ee6a45acec4b661544f227f746bd13e8
Fedor Savin <Savin20153@gmail.com>
2025-02-13 23:33:54 +0300

    change the parameters

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,23 +1,27 @@
+import prompt
+
 from brain_games import cli
 
+NUMBER_OF_GAME_LAUNCERS = 3
 
-def game_play(QUESTION, game):
+
+def game_play(game_module):
     name = cli.welcome_user()
-    print(QUESTION)
-    score = 0
-    for _ in range(3):
-        ques_num, right_ans = game.ques_num_and_ans()
+    print(game_module.QUESTION)
+    for game_launch in range(NUMBER_OF_GAME_LAUNCERS):
+        ques_num, right_ans = game_module.question_number_and_answer()
         print(f'Question: {ques_num}')
-        user_ans = input('Your answer: ')
+        user_ans = prompt.string('Your answer: ')
         if user_ans == right_ans:
             print('Correct!')
-            score += 1
         else:
             print(
-                f"'{user_ans}' is wrong answer ;(. "
+                f"'{user_ans}' is wrong answer ;(\n"
                 f"Correct answer was '{right_ans}'."
             )
             print(f"Let's try again, {name}!")
             return
-    if score == 3:
-        print(f'Congratulations, {name}!')
+    print(f'Congratulations, {name}!')
+
+
+
